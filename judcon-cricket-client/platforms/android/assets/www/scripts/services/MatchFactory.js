4a155823a2fc36ef002172690f30ed8c347d47e9
sebastienblanc <scm.blanc@gmail.com>
2014-01-16 21:57:01 +0100

    hack for passing port and show score

--- judcon-cricket-client/platforms/android/assets/www/scripts/services/MatchFactory.js
+++ judcon-cricket-client/platforms/android/assets/www/scripts/services/MatchFactory.js
@@ -1,4 +1,4 @@
 angular.module('judconcricket').factory('MatchResource', function($resource){
-    var resource = $resource('rest/matchs/:MatchId',{MatchId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
+    var resource = $resource('http://192.168.1.43:8080\:8080/judcon-cricket/rest/matchs/:MatchId',{MatchId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
     return resource;
 });
